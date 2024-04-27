fe0d926d29e01e70bef26bf988d8e58c2f2a0b2c
EnzignoS <148708901+EnzignoS@users.noreply.github.com>
2024-04-27 21:21:49 +0200

    gestione aste funzioni
    Aggiunta di funzioni:
    GestoreAste,
    CreaAsta,
    ModificaAsta,
    annullaAsta,
    effettuaOfferta

--- src/aste/server/GestoreAste.java
+++ src/aste/server/GestoreAste.java
@@ -1,5 +1,40 @@
 package aste.server;
 
+import java.net.InetAddress;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Hashtable;
+import java.util.concurrent.ScheduledExecutorService;
+
 public class GestoreAste {
 
+    private int numeroAsteCorrenti; 
+    private Hashtable<Integer, InetAddress> mappaIndirizzi;
+    private ScheduledExecutorService executorScheduler;
+    
+    
+    // GestoreAste(threadPoolAste : int)
+    public GestoreAste(int threadPoolAste) {
+
+    }
+
+    // creaAsta(prezzoInizio : int, dataOraInizio : LocalDateTime, durata : LocalTime, astaAutomatica : boolean, rifLotto : int)
+    public void creaAsta(int prezzoInizio, LocalDateTime dataOraInizio, LocalTime durata, boolean astaAutomatica, int rifLotto) {
+
+    }
+
+    // modificaAsta(idAsta : int, prezzoInizio : int, dataOraInizio : LocalDateTime, durata : LocalTime, astaAutomatica : boolean, rifLotto : int)
+    public void modificaAsta(int idAsta, int prezzoInizio, LocalDateTime dataOraInizio, LocalTime durata, boolean astaAutomatica, int rifLotto) {
+
+    }
+
+    // annullaAsta(id : int, descrizioneAnnullamento : String)
+    public void annullaAsta(int annullaAsta, String descrizioneAnnullamento) {
+
+    }
+
+    // effettuaOfferta(idAsta : int, idUtente : int, valore : int)
+    public void effettuaOfferta(int idAsta, int idUtente, int valore) {
+
+    }
 }
