31601d8c89b2db57f837bc6543e4adf4d12b34b7
Chaos-Rat <120569014+Chaos-Rat@users.noreply.github.com>
2024-04-27 17:28:19 +0200

    Finita classe Risposta

--- src/aste/Risposta.java
+++ src/aste/Risposta.java
@@ -1,5 +1,21 @@
 package aste;
 
-public class Risposta {
+import java.io.Serializable;
 
+public class Risposta implements Serializable {
+	public TipoRisposta tipoRisposta;
+	public Object[] payload;
+	
+	private static final long serialVersionUID = -1993048461030574444L;
+
+	public enum TipoRisposta {
+		OK,
+		ERRORE
+	}
+
+	public enum TipoErrore {
+		CAMPI_INVALIDI,
+		OPERAZIONE_INVALIDA,
+		GENERICO
+	}
 }
