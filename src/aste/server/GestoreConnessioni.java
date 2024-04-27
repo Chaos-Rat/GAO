1f07f8b2b49b60ccca829b4e94e7e4a88920a040
EnzignoS <148708901+EnzignoS@users.noreply.github.com>
2024-04-27 21:50:08 +0200

    aggiunta funzioni in GestoreConnessioni
    aggiunta di GestoreConnesioni e avvia

--- src/aste/server/GestoreConnessioni.java
+++ src/aste/server/GestoreConnessioni.java
@@ -1,5 +1,20 @@
 package aste.server;
 
+import java.net.ServerSocket;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class GestoreConnessioni {
-	
+
+    private GestoreAste gestoreAste; 
+    private ServerSocket serverSocket; 
+    private ThreadPoolExecutor poolConnession; 
+
+
+    public GestoreConnessioni(int threadPoolConnessioni, GestoreAste gestoreAste) {
+
+    }
+
+    public void avvia() {
+        
+    }
 }
