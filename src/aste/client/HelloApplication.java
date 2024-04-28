a9fb27d925c3859d109fd9c7ff533ffc92363634
Ashura2004 <marwansherif04@gmail.com>
2024-04-28 22:54:05 +0200

    GestoreClient.logout Server method finished, Client Login,Registration Method implemented

--- src/aste/client/HelloApplication.java
+++ src/aste/client/HelloApplication.java
@@ -7,10 +7,30 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application
+{
+    private static Socket socket;
+    public static ObjectInputStream input;
+    public static ObjectOutputStream output;
+
+    static {
+        try {
+            socket = new Socket("localhost",3000);
+            input = new ObjectInputStream(socket.getInputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         Image image = new Image(getClass().getResourceAsStream("view/Screenshot.png"));
