8377f660998a49fcd64db2864a921beb87796b55
Ashura2004 <marwansherif04@gmail.com>
2024-05-05 11:31:46 +0200

    Added CreaAsta method missing 2 variables

--- src/aste/client/controller/AstaController.java
+++ src/aste/client/controller/AstaController.java
@@ -69,8 +69,13 @@ public class AstaController
     private Spinner<LocalTime>spinner;
 
     @FXML
+    private TextField priceF;
+
+    @FXML
     public void initialize() throws IOException, ClassNotFoundException
     {
+        category.setVisible(false);
+
         spinner.setEditable(true);
         SpinnerValueFactory<LocalTime> factory = new SpinnerValueFactory<LocalTime>() {
             {
@@ -95,14 +100,42 @@ public class AstaController
 
     }
     @FXML
-    void CreateClicked(ActionEvent event)
-    {
+    void CreateClicked(ActionEvent event) throws IOException, ClassNotFoundException {
         LocalDate startDate = dateF.getValue();
         LocalTime time = spinner.getValue();
         String datetimestr = (startDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))+" "+time.format(DateTimeFormatter.ofPattern("HH:mm")));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
         LocalDateTime dateTime = LocalDateTime.parse(datetimestr,formatter);
         System.out.println((String) dateTime.format(formatter));
+        Richiesta richiestaCrea = new Richiesta();
+        richiestaCrea.tipoRichiesta = Richiesta.TipoRichiesta.CREA_ASTA;
+        richiestaCrea.payload = new Object[5];
+        richiestaCrea.payload[0] = dateTime;
+        richiestaCrea.payload[1] = 0;
+        richiestaCrea.payload[2] = Float.valueOf(priceF.getText());
+//        richiestaCrea.payload[3] = ;
+//        richiestaCrea.payload[4] = ;
+        HelloApplication.output.writeObject(richiestaCrea);
+        Risposta rispostaCrea = (Risposta) HelloApplication.input.readObject();
+        if (rispostaCrea.tipoRisposta == Risposta.TipoRisposta.OK) 
+        {
+            System.out.println("è stato creato l'asta con successo|");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Aste.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("The AuctionHouse");
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+            Stage stage1 = (Stage) createB.getScene().getWindow();
+            stage1.close();
+        } else if (rispostaCrea.payload[0] == Risposta.TipoErrore.OPERAZIONE_INVALIDA || rispostaCrea.payload[0] == Risposta.TipoErrore.CAMPI_INVALIDI ||rispostaCrea.payload[0] == Risposta.TipoErrore.GENERICO)
+        {
+            System.out.println(rispostaCrea.payload[0]);
+        }
+
+
     }
     @FXML
     void ArticoliClicked(ActionEvent event) throws IOException
