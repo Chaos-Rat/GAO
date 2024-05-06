87b3779fc926eb64392d6e4d80786f2ae2cd483c
Ashura2004 <marwansherif04@gmail.com>
2024-05-06 22:35:14 +0200

    VisualizzaAsta works fully now

--- src/aste/client/controller/HomeController.java
+++ src/aste/client/controller/HomeController.java
@@ -10,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -25,6 +26,8 @@ import javafx.stage.Stage;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
 
 public class HomeController {
 
@@ -61,6 +64,8 @@ public class HomeController {
     @FXML
     private Text Title;
 
+    @FXML
+    private ComboBox<String>category;
 
     @FXML
     private VBox vbox;
@@ -87,7 +92,78 @@ public class HomeController {
     }
 
     @FXML
-    public void initialize() throws IOException, ClassNotFoundException {
+    public void initialize() throws IOException, ClassNotFoundException
+    {
+        category.setVisible(false);
+        Richiesta richiestacat = new Richiesta();
+        richiestacat.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_CATEGORIE;
+        HelloApplication.output.writeObject(richiestacat);
+        Risposta rispostacat = (Risposta) HelloApplication.input.readObject();
+        HashMap<String, Integer> catmap = new HashMap<String, Integer>();
+        if (rispostacat.tipoRisposta == Risposta.TipoRisposta.OK)
+        {
+            for (int i = 0 ; i < rispostacat.payload.length/2 ; i++)
+            {
+                catmap.put((String) rispostacat.payload[i*2+1], (Integer) rispostacat.payload[i*2]);
+            }
+            catmap.put("Tutte le categorie",0);
+            category.getSelectionModel().select("Altre categorie");
+            category.getItems().addAll(catmap.keySet());
+        }
+        Richiesta richiestaAste = new Richiesta();
+        richiestaAste.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_ASTE;
+        richiestaAste.payload = new Object[4];
+        richiestaAste.payload[0] = 10;
+        richiestaAste.payload[1] = 1;
+        richiestaAste.payload[2] = "";
+        richiestaAste.payload[3] = catmap.get(category.getSelectionModel().getSelectedItem());
+        HelloApplication.output.writeObject(richiestaAste);
+        Risposta rispostaAste = (Risposta) HelloApplication.input.readObject();
+        if (rispostaAste.tipoRisposta == Risposta.TipoRisposta.OK)
+        {
+            for (int i = 0; i < rispostaAste.payload.length/5; i++)
+            {
+                HBox box = new HBox();
+                Integer idAsta = (Integer) rispostaAste.payload[i*5+0];
+                Duration duration = (Duration)rispostaAste.payload[i*5+1];
+                Float price = (Float)rispostaAste.payload[i*5+2];
+                String Lottoname = (String)rispostaAste.payload[i*5+3];
+                FileOutputStream out = new FileOutputStream("cache/Articolo.png");
+                out.write((byte[]) rispostaAste.payload[i*5+4]);
+                out.close();
+                FileInputStream in = new FileInputStream("cache/Articolo.png");
+                Image img = new Image(in);
+                in.close();
+                ImageView item = new ImageView();
+                item.setImage(img);
+                item.setFitWidth(100);
+                item.setFitHeight(100);
+                item.setPreserveRatio(true);
+                Text nomeT = new Text("LottoNome: " + Lottoname);
+                Text priceT = new Text("StartingPrice : " + price.toString());
+                nomeT.setWrappingWidth(150);
+                priceT.setWrappingWidth(150);
+                VBox vbox = new VBox();
+                VBox vbox2 = new VBox();
+                VBox vbox3 = new VBox();
+                Button button = new Button();
+                vbox.setAlignment(Pos.CENTER);
+                vbox2.setAlignment(Pos.CENTER);
+                vbox3.setAlignment(Pos.CENTER);
+                vbox.getChildren().add(item);
+                vbox2.getChildren().addAll(nomeT,priceT);
+                vbox3.getChildren().addAll(button);
+                box.setSpacing(50);
+                box.setPrefWidth(940);
+                box.setAlignment(Pos.CENTER);
+                box.getChildren().addAll(vbox, vbox2);
+                asteList.getChildren().add(box);
+            }
+        } else if (rispostaAste.payload[0]== Risposta.TipoRisposta.ERRORE)
+        {
+            System.out.println(rispostaAste.payload[0]);
+            System.out.println((rispostaAste.payload[1]));
+        }
         Richiesta richiesta = new Richiesta();
         richiesta.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_IMMAGINE_PROFILO;
         richiesta.payload = new Object[]{0};
