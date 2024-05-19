96202fec04e04fed66a77c818642f6a6732b186d
Ashura2004 <marwansherif04@gmail.com>
2024-05-19 11:12:38 +0200

    ArticoloDetailsController Error (Needs fixing)

--- src/aste/client/controller/ArticoloDetailsController.java
+++ src/aste/client/controller/ArticoloDetailsController.java
@@ -3,6 +3,7 @@ package aste.client.controller;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
 
 import aste.Richiesta;
 import aste.Risposta;
@@ -20,6 +21,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -84,36 +86,85 @@ public class ArticoloDetailsController {
 	public static Integer idArticolo;
 
 	@FXML
-	public void initialize() throws IOException, ClassNotFoundException {
-//		System.out.println(idArticolo);
-//        useridText.setUnderline(true);
-//        Richiesta richiestaArticolo = new Richiesta();
-//        richiestaArticolo.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_ARTICOLO;
-//        richiestaArticolo.payload = new Object[1];
-//        richiestaArticolo.payload[0] = idArticolo;
-//        HelloApplication.output.writeObject(richiestaArticolo);
-//        Risposta rispostaArticolo = (Risposta) HelloApplication.input.readObject();
-//        if (rispostaArticolo.tipoRisposta == Risposta.TipoRisposta.OK)
-//        {
-//            nomeText.setText((String) rispostaArticolo.payload[0]);
-//            condText.setText((String) rispostaArticolo.payload[1]);
-//            descText.setText((String) rispostaArticolo.payload[2]);
-//            lottoText.setText((String) rispostaArticolo.payload[3]);
-//            FileOutputStream out = new FileOutputStream("cache/Articolo.png");
-//            out.write((byte[]) rispostaArticolo.payload[4]);
-//            out.close();
-//            FileInputStream in = new FileInputStream("cache/Articolo.png");
-//            Image img = new Image(in);
-//            articolo.setImage(img);
-//            in.close();
-//            useridText.setText((String) rispostaArticolo.payload[5]);
-//            emailText.setText((String) rispostaArticolo.payload[6]);
-//
-//        } else if (rispostaArticolo.tipoRisposta == Risposta.TipoRisposta.ERRORE)
-//        {
-//            System.out.println(rispostaArticolo.payload[0]);
-//            System.out.println(rispostaArticolo.payload[1]);
-//        }
+	public void initialize() throws IOException, ClassNotFoundException
+    {
+        Richiesta richiesta = new Richiesta();
+        richiesta.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_IMMAGINE_PROFILO;
+        richiesta.payload = new Object[]{0};
+        HelloApplication.output.writeObject(richiesta);
+        Risposta risposta = (Risposta) HelloApplication.input.readObject();
+        if (risposta.tipoRisposta == Risposta.TipoRisposta.OK) {
+            FileOutputStream picture = new FileOutputStream("imagine.png");
+            picture.write((byte[]) risposta.payload[0]);
+            picture.close();
+            FileInputStream defaultImg = new FileInputStream("imagine.png");
+            Image image = new Image(defaultImg);
+            ImageView imageView = new ImageView();
+            imageView.setImage(image);
+            avatar.setFill(new ImagePattern(imageView.getImage()));
+            defaultImg.close();
+        }
+        else
+        {
+            System.out.println(risposta.tipoRisposta.toString());
+            System.out.println(((Risposta.TipoErrore) risposta.payload[0]).toString());
+        }
+        Richiesta richiestaProfile = new Richiesta();
+        richiestaProfile.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_PROFILO;
+        richiestaProfile.payload = new Object[1];
+        richiestaProfile.payload[0] = 0;
+        HelloApplication.output.writeObject(richiestaProfile);
+        Risposta rispostaProfile = (Risposta) HelloApplication.input.readObject();
+        if (rispostaProfile.tipoRisposta == Risposta.TipoRisposta.OK)
+        {
+            String nome = (String) rispostaProfile.payload[0];
+            String cognome = (String) rispostaProfile.payload[1];
+            LocalDate birthdate = (LocalDate) rispostaProfile.payload[2];
+            String city = (String) rispostaProfile.payload[3];
+            Integer cap = (Integer) rispostaProfile.payload[4];
+            String address = (String) rispostaProfile.payload[5];
+            String email = (String) rispostaProfile.payload[6];
+            String iban = (String) rispostaProfile.payload[7];
+            String s1 = nome.substring(0,1).toUpperCase() + nome.substring(1);
+            String s2 = cognome.substring(0,1).toUpperCase() + cognome.substring(1);
+            username.setText(s1  + " " + s2);
+        } else if (rispostaProfile.tipoRisposta == Risposta.TipoRisposta.ERRORE)
+        {
+            System.out.println(rispostaProfile.payload[0]);
+        }
+		System.out.println(idArticolo);
+        useridText.setUnderline(true);
+        Richiesta richiestaArticolo = new Richiesta();
+        richiestaArticolo.tipoRichiesta = Richiesta.TipoRichiesta.VISUALIZZA_ARTICOLO;
+        richiestaArticolo.payload = new Object[1];
+        richiestaArticolo.payload[0] = idArticolo;
+        HelloApplication.output.writeObject(richiestaArticolo);
+        Risposta rispostaArticolo = (Risposta) HelloApplication.input.readObject();
+        if (rispostaArticolo.tipoRisposta == Risposta.TipoRisposta.OK)
+        {
+            nomeText.setText((String) rispostaArticolo.payload[0]);
+            condText.setText((String) rispostaArticolo.payload[1]);
+            descText.setText((String) rispostaArticolo.payload[2]);
+            lottoText.setText((String) rispostaArticolo.payload[3]);
+            Image [] image = new Image[((byte[][])rispostaArticolo.payload[4]).length];
+            for (int i = 0; i < image.length; i++)
+            {
+                FileOutputStream out = new FileOutputStream("cache/Articolo.png");
+                out.write(((byte[][]) rispostaArticolo.payload[4])[i]);
+                out.close();
+                FileInputStream in = new FileInputStream("cache/Articolo.png");
+                image [i] = new Image(in);
+                in.close();
+            }
+            articolo.setImage(image[0]);
+            useridText.setText((String) rispostaArticolo.payload[5]);
+            emailText.setText((String) rispostaArticolo.payload[6]);
+
+        } else if (rispostaArticolo.tipoRisposta == Risposta.TipoRisposta.ERRORE)
+        {
+            System.out.println(rispostaArticolo.payload[0]);
+            System.out.println(rispostaArticolo.payload[1]);
+        }
     }
     @FXML
     void ArticoliClicked(ActionEvent event) throws IOException 
